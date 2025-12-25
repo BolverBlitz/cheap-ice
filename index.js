aec10df490c9db6a8445d0435b21ce5cbadcedb7
Marc <bolverblitz@ebg.pw>
2025-12-25 18:29:08 +0100

    Add video generation

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 require('dotenv').config({ quiet: true });
 const ProjectManager = require('./lib/projectManager.js');
 const IngressIceReplica = require('./lib/ice.js');
+const VideoGenerator = require('./lib/ffmpeg.js');
+
+const screenshotDir = process.env.SCREENSHOT_DIR || './screenshots';
 
 const pm = new ProjectManager(process.cwd());
+const ffmpeg = new VideoGenerator(parseInt(process.env.TARGET_FPS) || 30, pm.projectDir, screenshotDir);
 
 (async () => {
-    if(process.env.DEBUG == 'true' ) console.log("Running in DEBUG mode.");
-    const config = await pm.getConfiguration();
+    if (process.env.DEBUG == 'true') console.log("Running in DEBUG mode.");
+    const { config, command } = await pm.getConfiguration();
 
-    const screenshotDir = process.env.SCREENSHOT_DIR || './screenshots';
+    if (command !== 'skip') {
+        const iceBot = new IngressIceReplica(
+            config.intelUrl,
+            screenshotDir,
+            config.screenshotInterval,
+            config.numberOfScreenshots,
+            config.screenshotWidth,
+            config.screenshotHeight
+        );
 
-    const iceBot = new IngressIceReplica(
-        config.intelUrl,
-        screenshotDir,
-        config.screenshotInterval,
-        config.numberOfScreenshots,
-        config.screenshotWidth,
-        config.screenshotHeight
-    );
+        pm.listenForStopCommand(() => {
+            iceBot.stop();
+        });
 
-    pm.listenForStopCommand(() => {
-        iceBot.stop();
-    });
+        await iceBot.run();
+    }
 
-    await iceBot.run();
+    const ffmpegAvailable = await ffmpeg.checkFFmpeg();
+    if (!ffmpegAvailable) {
+        console.error("FFmpeg is not available. Skipping video generation.");
+        process.exit(1);
+    }
 
-    console.log("All tasks completed.");
-
+    await ffmpeg.generateVideo(config.screenshotInterval);
 })();
