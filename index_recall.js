ba67786185961bb78e36cca8c3c4a1a095681d56
Marc <bolverblitz@ebg.pw>
2026-01-03 22:41:16 +0100

    Fix some ghost links and add action based simulation

--- index_recall.js
+++ index_recall.js
@@ -12,7 +12,7 @@ const ffmpeg = new VideoGenerator(parseInt(process.env.TARGET_FPS) || 30, pm.pro
     if (process.env.DEBUG == 'true') console.log("Running in DEBUG mode.");
     const { config, command } = await pm.getConfiguration();
 
-    if(config.numberOfScreenshots === 0) {
+    if (config.numberOfScreenshots === 0 && config.screenshotPerAction !== true) {
         console.error("For history simulation, 'Run until stopped' is not supported. Please specify a finite number of screenshots or duration.");
         process.exit(1);
     }
@@ -22,17 +22,24 @@ const ffmpeg = new VideoGenerator(parseInt(process.env.TARGET_FPS) || 30, pm.pro
         screenshotDir,
     );
 
+    // Current time minus config.numberOfScreenshots * config.screenshotInterval or historyContextWindow (in days) whatever is larger
+    const screenshotsMs = config.numberOfScreenshots * config.screenshotInterval * 1000;
+    const historyContextMs = (config.historyContextWindow || 0) * 24 * 60 * 60 * 1000;
+    const totalLookback = Math.max(screenshotsMs, historyContextMs);
+    const getDataUntilTimestamp = Date.now() - totalLookback;
+    await iceBot.setSimulationStart(historyContextMs, screenshotsMs)
+
     if (command !== 'skip') {
-        // Current time minus config.numberOfScreenshots * config.screenshotInterval
-        const getDataUntilTimestamp = Date.now() - (config.numberOfScreenshots * config.screenshotInterval * 1000);
         await iceBot.fetchHistoryUntil(getDataUntilTimestamp);
     }
 
     await iceBot.simulateHistory(
         screenshotDir,
         config.screenshotInterval,
+        config.screenshotTimestamp,
         config.screenshotWidth,
-        config.screenshotHeight
+        config.screenshotHeight,
+        config.screenshotPerAction
     );
 
     const ffmpegAvailable = await ffmpeg.checkFFmpeg();
